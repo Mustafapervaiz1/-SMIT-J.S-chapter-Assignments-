0bb3a58ac7c5c2585074011021a7d13ab16634e8
mustafapervez <syalani.training1@gmail.com>
2023-11-02 00:16:16 +0500

    second commit

--- ch5/mob.js
+++ ch5/mob.js
@@ -4,4 +4,26 @@ var a = +prompt("enter first value ")
 var b = +prompt("enter second value ")
 
 
-document.write(`sum of ${+a} and ${+b} is ${a+b}`)
+document.write(`sum of ${+a} and ${+b} is ${a+b}<br>`)
+
+//Repeat task1 for subtraction, multiplication, division & modulus.
+
+var c = +prompt("enter first value for subtraction")
+var d = +prompt("enter first value for subtraction")
+document.write(`  ${c} - ${d} = ${c-d}<br><br> `)
+
+var e = +prompt("enter first value for multiplication")
+var f = +prompt("enter first value for multiplication")
+document.write(`  ${e} * ${f} = ${e*f} <br><br>`)
+
+
+var g = +prompt("enter first value for division")
+var h = +prompt("enter first value for division")
+document.write(` ${g} / ${h} = ${g/h} <br><br>`)
+
+
+var i = +prompt("enter first value for modulus")
+var j = +prompt("enter first value for modulus")
+document.write(`  ${g} % ${h} = ${g%h} <br><br>`)
+
+
